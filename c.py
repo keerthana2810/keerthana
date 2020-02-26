31a12dd1cae1c7c4c00e85035c34003b007729d0
keerthana2810 <61489811+keerthana2810@users.noreply.github.com>
2020-02-26 11:23:59 +0530

    py

---
+++ c.py
@@ -0,0 +1,42 @@
+a = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9
+print(a)
+
+c = (100 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9)
+print(c)
+
+
+d = [1000 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9]
+
+print(d)
+
+e = {2000 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9}
+
+print(e)
+
+SAM = ['a','b','c']
+print(SAM)
+sam = ['A','B','cC']
+print(sam)
+
+S = ['aa',\
+     'bb',\
+     'cb']
+print(S)
+
+n= 10
+print(n)
+n = 100
+print(n)
+
+x = y=xy =1001
+print(x)
+print(y)
+print(xy)
